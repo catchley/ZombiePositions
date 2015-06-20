50e21b99d585dd7052f6841575b132d38125218e
Chris Atchley <steve.chris.atchley@gmail.com>
2015-06-20 01:21:41 -0400

    Added Ads

--- app/src/main/java/atchley/chris/zombiepositions/MainActivity.java
+++ app/src/main/java/atchley/chris/zombiepositions/MainActivity.java
@@ -8,31 +8,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class MainActivity extends ActionBarActivity {
     protected Button mFourPlayerButton;
     protected Button mSixPlayerButton;
-
-
-
+    InterstitialAd mInterstitialAd;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFourPlayerButton = (Button)findViewById(R.id.fourPlayerButton);
-        mSixPlayerButton = (Button)findViewById(R.id.sixPlayerButton);
+        mFourPlayerButton = (Button) findViewById(R.id.fourPlayerButton);
+        mSixPlayerButton = (Button) findViewById(R.id.sixPlayerButton);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-1576273189830832/9269882302");
+        requestNewInterstitial();
 
 
         //Button click opens 4 player Chooser activity
         mFourPlayerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
-                Intent intent = new Intent(MainActivity.this,FourPlayerChooserActivity.class);
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, FourPlayerChooserActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                Intent intent = new Intent(MainActivity.this, FourPlayerChooserActivity.class);
                 startActivity(intent);
             }
         });
@@ -42,13 +56,33 @@ public class MainActivity extends ActionBarActivity {
         mSixPlayerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,SixPlayerActivity.class);
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                Intent intent = new Intent(MainActivity.this, SixPlayerActivity.class);
+                startActivity(intent);
+            }}
+
+        });
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                Intent intent = new Intent(MainActivity.this, SixPlayerActivity.class);
                 startActivity(intent);
             }
         });
 
 
     }
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("YOUR_DEVICE_HASH")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
 
 
-   }
+
+}
